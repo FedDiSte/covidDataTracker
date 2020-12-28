6e536f720f423df68fa552d43fdf20118744106d
FedDiSte <33763790+FedDiSte@users.noreply.github.com>
2020-12-28 21:45:13 +0100

    Update, finalmente funziona

--- test/test.php
+++ test/test.php
@@ -1,29 +1,46 @@
 <?php
-  $curl = curl_init();
-  if(file_exists('creato.csv')) {
-    file_delete('creato.csv');
+  function logger($qualcosa) {
+    $output = $qualcosa;
+    if(is_array($output)) {
+      $output = implode(',', $output);
+    }
+    echo "<script>console.log('Logging: ".$output."' );</script>";
   }
-  $file = fopen('creato.csv', 'w');
-  curl_setopt($curl, CURLOPT_URL, 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale-20201226.csv');
-  curl_setopt($curl, CURLOPT_HEADER, 0);
-  curl_setopt($curl, CURLOPT_FILE, $file);
-  curl_exec($curl);
-  fclose($file);
 
-  $row = 1;
-  echo "<br>pre if";
-  if(($handle = fopen('creato.csv', 'r')) !== FALSE) {
-    echo "in if";
-    while(($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-        echo "in while";
-        $num = count($data);
-        echo "<p> $num fields in line $row: <br /></p>\n";
-        //$row++;
-        for ($c=0; $c < $num; $c++) {
-            echo "in for";
-            echo $data[$c] . "<br />\n";
+  for($i = 0; $i < 10; $i++) {
+    $curl = curl_init();
+    $file = fopen('creato.csv', 'w');
+    if(date("H:i") > strtotime("17:00")) {
+      $today = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale-".date("Ymd", strtotime("-".$i." days")).".csv";
+    } else {
+      $incremento = $i + 1;
+      $today = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale-".date("Ymd", strtotime("-".$incremento." days")).".csv";
+    }
+    curl_setopt($curl, CURLOPT_URL, $today);
+    curl_setopt($curl, CURLOPT_HEADER, 0);
+    curl_setopt($curl, CURLOPT_FILE, $file);
+    curl_exec($curl);
+    fclose($file);
+
+    $datiLetti = array();
+    if (($handle = fopen("creato.csv", "r")) !== FALSE) {
+        while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+            $datiLetti[] = $data;
         }
         fclose($handle);
-      }
+    }
+
+    $totalePositivi = array();
+    $nuoviPositivi = array();
+    $rapportoPositiviSuTamponi = array();
+
+    logger("Data: ".$datiLetti[1][0]);
+    logger("Totale positivi: ".$datiLetti[1][6]);
+    $totalePositivi[$i] = $datiLetti[1][6];
+    logger("Nuovi positivi: ".$datiLetti[1][7]);
+    $nuoviPositivi[$i] = $datiLetti[1][7];
+    logger("Rapporto positivi su tampomi: ".$datiLetti[1][14]/$datiLetti[1][13]);
+    $rapportoPositiviSuTamponi[$i] = $datiLetti[1][14]/$datiLetti[1][13];
   }
+
 ?>
