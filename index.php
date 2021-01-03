d33d67027c6a7a3d41221a48223a527fc84794d9
FedDiSte <33763790+FedDiSte@users.noreply.github.com>
2021-01-03 16:06:49 +0100

    aggiunta .gitignore e prime modifiche generali su pagina index

--- index.php
+++ index.php
@@ -36,19 +36,15 @@
 <!DOCTYPE HTML>
 <html>
 <head>
+  <title>covidDataTracker</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="mdl/material.min.css">
   <script src="mdl/material.min.js"></script>
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
   <style>
-    .card-square.mdl-card {
-      width: 320px;
-      height: 320px;
-    }
     .card-square > .mdl-card__title {
       color: #fff;
-      background:
-      url('../assets/demos/dog.png') bottom right 15% no-repeat #46B6AC;
+      background: #46B6AC;
     }
   </style>
 </head>
@@ -69,9 +65,54 @@
     <main class="mdl-layout__content">
       <div class="page-content">
         <div class="mdl-grid">
-          <div class="mdl-cell mdl-cell--12-col">
-            
+          <div class="mdl-cell mdl-cell--4-col">
+            <div class="card-square mdl-card--border mdl-shadow--4dp">
+              <div class="mdl-card__title mdl-card--expand">
+                <h2 class="mdl-card__title-text">Totale positivi</h2>
+              </div>
+              <div class="mdl-card__supporting-text">
+                Totale positivi: <?php echo $datiLetti[1][6]?>
+              </div>
+              <div class="mdl-card__actions mdl-card--border">
+                <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+                  Resoconto ultimi 10 giorni
+                </a>
+              </div>
+            </div>
           </div>
+          <div class="mdl-cell mdl-cell--4-col">
+            <div class="card-square mdl-card--border mdl-shadow--4dp">
+              <div class="mdl-card__title mdl-card--expand">
+                <h2 class="mdl-card__title-text">Nuovi positivi</h2>
+              </div>
+              <div class="mdl-card__supporting-text">
+                Nuovi positivi: <?php echo $datiLetti[1][8]?>
+              </div>
+              <div class="mdl-card__actions mdl-card--border">
+                <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+                  Resoconto ultimi 10 giorni
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="mdl-cell mdl-cell--4-col">
+            <div class="card-square mdl-card--border mdl-shadow--4dp">
+              <div class="mdl-card__title mdl-card--expand">
+                <h2 class="mdl-card__title-text">Tamponi</h2>
+              </div>
+              <div class="mdl-card__supporting-text">
+                Totale tamponi: <?php echo $datiLetti[1][14]?>
+              </div>
+              <div class="mdl-card__actions mdl-card--border">
+                <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+                  Resoconto ultimi 10 giorni
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="mdl-grid">
+
         </div>
     </main>
   </div>
